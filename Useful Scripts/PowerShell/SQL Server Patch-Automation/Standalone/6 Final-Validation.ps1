d55e6bfb896185421697155a8e52556e5d592eee
Bugzbaggy <92191389+Bugzbaggy@users.noreply.github.com>
2023-04-12 22:30:28 +0800

    Add files via upload

--- Useful Scripts/PowerShell/SQL Server Patch-Automation/Standalone/6 Final-Validation.ps1
+++ Useful Scripts/PowerShell/SQL Server Patch-Automation/Standalone/6 Final-Validation.ps1
@@ -1,35 +1,45 @@
-$servers = Get-Content "C:\Temp\primary.txt"
+$servers = Get-Content "C:\Temp\sa.txt"
 
 #$sqlpatch = “D:\SQLServer2017-KB5021126-x64.exe” 
 
 $outputarray = @()
+$InstanceName = @()
 ForEach($server in $servers){
     #$Service = Get-service -ComputerName $server | where {($_.displayname -like "SQL Server (*") }
     
-   $SQLService = (Get-service -ComputerName $server | where {($_.displayname -like "SQL Server (*") }).Name
-   $ServerInstanceSplit = $SQLService.Split("$")
+   $SQLServices = (Get-service -ComputerName $server | where {($_.displayname -like "SQL Server (*") }).Name
+   
     #$Instance = ($server |% {Get-ChildItem -Path "SQLSERVER:\SQL\$_"}).Name
     $SQLVersion = (Invoke-Sqlcmd -Query "SELECT SUBSTRING(@@VERSION,0,68);" -ServerInstance $InstanceName).Column1
     $LogTime = Get-Date -Format "yyyy-MM-dd hh:mm:ss"
-    $InstanceName = $server + '\' + $ServerInstanceSplit[1]
-	$DBstate = Invoke-Sqlcmd -Query "SELECT name as DBName, state_desc AS Status
-	FROM sys.databases db
-	WHERE db.database_id NOT IN ('1','2','3','4')" -ServerInstance "$InstanceName"
-    
-       $Objt = New-Object PSObject
+
+    Foreach($SQLService in $SQLServices){
+    $ServerInstanceSplit = $SQLService.Split("$")
+    $InstanceName += $server + '\' + $ServerInstanceSplit[1]
+    }
+	
+		
+	ForEach($Instance in $InstanceName){
+       
+       #If($InstanceName -eq $DBstate.DBName){
+
+       $DBstates = Invoke-Sqlcmd -Query "SELECT name as DBName, state_desc AS Status
+	    FROM sys.databases db
+	    WHERE db.database_id NOT IN ('1','2','3','4')" -ServerInstance "$Instance"
+	   $Objt = New-Object PSObject
        $Objt | Add-Member -MemberType NoteProperty -Name ComputerName -Value $server.ToUpper()
-       $Objt | Add-Member -MemberType NoteProperty -Name InstanceName -Value $InstanceName.ToUpper()
-       $Objt | Add-Member -MemberType NoteProperty -Name DBName -Value $DBstate.DBName.ToUpper()
-       $Objt | Add-Member -MemberType NoteProperty -Name Status -Value $DBstate.Status.ToUpper()
+       $Objt | Add-Member -MemberType NoteProperty -Name InstanceName -Value $Instance.ToUpper()
+       $Objt | Add-Member -MemberType NoteProperty -Name DBName -Value $DBstates.DBName.ToUpper()
+       $Objt | Add-Member -MemberType NoteProperty -Name Status -Value $DBstates.Status.ToUpper()
        $Objt | Add-Member -MemberType NoteProperty -Name LogTime -Value $LogTime
        $Objt | Add-Member -MemberType NoteProperty -Name SQLVersion -Value $SQLVersion
        $outputarray += $Objt
-
-             
-       }
-
-   Write-Output $outputarray
-   $outputarray | Export-Csv -path "D:\SA_PatchVersionLatest.csv" -nti
-   notepad 'D:\SA_PatchVersionLatest.csv'
+    }   
+	}
+    #}
+#outputarray | FT
+Write-Output $outputarray
+$outputarray | Export-Csv -path "D:\SA_PatchVersionLatest.csv" -nti
+notepad 'D:\SA_PatchVersionLatest.csv'
    
    
