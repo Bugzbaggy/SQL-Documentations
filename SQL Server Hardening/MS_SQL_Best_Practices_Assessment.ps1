e7d279572b5902c79fc46e8eb5c47ac615072520
Bugzbaggy <92191389+Bugzbaggy@users.noreply.github.com>
2023-05-08 18:20:07 +0800

    Add files via upload

---
+++ SQL Server Hardening/MS_SQL_Best_Practices_Assessment.ps1
@@ -0,0 +1,67 @@
+<#
+    .NOTES 
+    Name: SQL Assessment API
+    Author: Renz Marion Bagasbas
+	            
+    .DESCRIPTION 
+        Microsoft's Best Practices Checker provides a mechanism to evaluate the configuration of SQL Servers, Azure SQL VMs, and Managed Instances for best practices both server and database level.
+
+        YOU WILL NEED TO LIST DOWN ALL SQL SERVERS IN THE SOURCE FILE
+#> 
+
+
+$dbservers = Get-Content "C:\Temp\test.txt" #List of all SQL servers
+
+$outputarray = @()
+ForEach($server in $dbservers){
+    Write-host -nonewline "."
+    $SQLService = (Get-service -ComputerName $server | where {($_.displayname -like "SQL Server (*") }).Name
+    $ServerInstanceSplit = $SQLService.Split("$")
+    $InstanceName = $server + '\' + $ServerInstanceSplit[1]
+    
+        $StartDate = Get-Date
+		Write-Host "
+		##########################################
+		SQL Assessment initiated...
+		Time Script Started $StartDate" -ForegroundColor Green
+
+       If($Service.Status -ne "Running"){
+		
+        Start-Service -Name $SQLService
+        # Running server scope rules
+		Get-SqlInstance -ServerInstance $($InstanceName) | 
+		Invoke-SqlAssessment -FlattenOutput |
+		Write-SqlTableData -ServerInstance 'azsapqdb01\QE2DB' -DatabaseName SQLAssessment -SchemaName bpc -TableName Results -Force #Change the SQL server instance preferred
+
+		# Running database scope rules
+		Get-SqlDatabase -ServerInstance $($InstanceName) | 
+		Invoke-SqlAssessment -FlattenOutput |
+		Write-SqlTableData -ServerInstance 'azsapqdb01\QE2DB' -DatabaseName SQLAssessment -SchemaName bpc -TableName Results -Force #Change the SQL server instance preferred
+
+       }
+       ElseIf($Service.Status -eq "Running"){
+       
+        # Running server scope rules
+		Get-SqlInstance -ServerInstance $($InstanceName) | 
+		Invoke-SqlAssessment -FlattenOutput |
+		Write-SqlTableData -ServerInstance 'azsapqdb01\QE2DB' -DatabaseName SQLAssessment -SchemaName bpc -TableName Results -Force
+
+		# Running database scope rules
+		Get-SqlDatabase -ServerInstance $($InstanceName) | 
+		Invoke-SqlAssessment -FlattenOutput |
+		Write-SqlTableData -ServerInstance 'azsapqdb01\QE2DB' -DatabaseName SQLAssessment -SchemaName bpc -TableName Results -Force
+        
+       }
+	
+    $EndDate = Get-Date
+    $Time = $EndDate - $StartDate
+    Write-Host "
+    ##########################################
+    Assessment results saved...
+    Time Script ended at $EndDate and took
+    $Time" -ForegroundColor Green	  
+    
+    }
+
+   
+
